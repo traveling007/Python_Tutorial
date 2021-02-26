a3629e7fd27baaa2aa7e6346026a7948c451c102
traveling007 <79721172+traveling007@users.noreply.github.com>
2021-02-26 22:34:40 +0200

    Deneme
    Deneme2

---
+++ 02-Deneme2.py
@@ -0,0 +1,8 @@
+print("Yeni calismalar")
+print("Hepiniz Hosgeldin")
+sayi=49
+sonuc=int(input("Girilen :"))
+if sonuc==sayi:
+    print("Girilen sayi sonuca esit")
+else:
+    print("Girilen sayi sonuca esit degil")
