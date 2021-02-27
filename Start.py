902beab1379ad8f751b62741b88e163f7599a0b0
traveling007 <79721172+traveling007@users.noreply.github.com>
2021-02-27 23:45:14 +0200

    start
    new lesson

--- Start.py
+++ Start.py
@@ -14,20 +14,26 @@ messages='software {}'      # format burada koseli parantezi kullanarak name de 
 
 
 
-##print(len(name)) ##kac karakter oldugunu gosterir
-##print(name[0:4])   0-4 arasinda ki karakteri yazdirir 3 dahil 4 dahil degil.
-## bosluk bir karakter olarak sayilir.
-##print(name[:4]) buda ayni sekilde 0-4 arasinda ki karakteri yazdirir 3 dahil 4 dahil degil
-##print(name.title()) ##bas harfleri buyuk yazar
-## print(name.upper))## tum yaziyi kucuk yazar 
-## print(name.lower)) ## tum yaziyi buyuk yazar 
-##print(name.count('a')) ## kac tane ornegin 'a' harfinden var gibi yani kac tane oldugunu yazdirir
-## print(name.find('n')) ##hangi karakterin kacinci sirada oldugunu gostermeye yarar
-##print(name.replace('ufuk bro' , 'adam')) ## burada hangi kelimeyi degistirmek istersek kullaniriz.
-##print(dir(name)) ##boyle yazdigimiz takdirde ilgili degiskenleri method ve ozellikleri bulabiliriz
-##print(help(str))## boyle yazdigimiz takdirde ilgili kullanbilcegimiz methodlari ve nelerde kullanabilecegimizi bize donen yardim dokumanidir
-##print(name[3]) ##bu sekilde yazarak kacinci karakterde hangi harf oldugunu bulur. 
-# unutmadan 0 ile basliyordu.
+##print(len(name))
+##print(name[0:3])  
+
+##print(name[:2]) 
+
+##print(name.title()) 
+
+##print(name.upper())
+
+##print(name.lower())  
+
+##print(name.count('m')) 
+
+##print(name.find('k'))
+ 
+##print(name.replace('mlk' , 'new')) 
+
+##print(dir(name)) 
+##print(help(str))
+
  
 
 
