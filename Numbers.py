1b2a04901834fdc60731f53c0d62fd3f003d21f1
traveling007 <79721172+traveling007@users.noreply.github.com>
2021-03-01 23:36:52 +0200

    numbers
    numbers class

---
+++ Numbers.py
@@ -0,0 +1,11 @@
+# Matematiksel islemler
+# Toplama 5 + 3
+# Cikarma 5 - 3
+# Carpma 5 * 3
+# Bolme 5 / 3
+# Tam Bolme 5 // 3
+# Us Alma 5 ** 3
+# Mod 5 % 3
+
+print(5-3*4+6)
+
---
+++ numbers2.py
@@ -0,0 +1,37 @@
+number=-5
+print(number)
+print(abs(number))
+
+number=6.78
+print(round(number))
+print(round(number,1))
+print(round(number,2))
+
+print(pow(8, 2)) # bir sayinin ussunu almak istersek kullanabiliriz
+
+print(min(5, 6, 3, 10)) # en kucuk sayi icin min 
+print(max(5, 6, 3, 10)) #en buyuk icin max kullaniriz
+
+number=4
+number +=2 
+number *=3
+number /=2
+
+print(number)
+
+num_1='24'
+num_2='40'
+
+print(num_1+num_2) #string
+
+num_1=24
+num_2=40
+
+print(int(num_1+num_2)) #int
+
+age=25
+message= 'Ben'+str(age)+'yasindayim' ## str olmaz ise mesage yazarken hata verir ve int oldugu icin 
+print(message)
+
+my_string='test'
+print(my_string*4)
