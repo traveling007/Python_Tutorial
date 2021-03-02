6a8c85075d9a32dc50c2f18c6c3658b5c4700345
traveling007 <79721172+traveling007@users.noreply.github.com>
2021-03-02 21:47:22 +0200

    information
    info

---
+++ infortmation.py
@@ -0,0 +1,36 @@
+print('denemeler')
+
+x='yeni baslangic'
+
+print(x)
+
+x='egitim basladi'
+
+print(x)
+
+name='mlk'
+
+messages='software {}'      # format burada koseli parantezi kullanarak name de ki tanimi almaya yariyor
+
+
+##information##
+
+
+##print(len(name)) ##kac karakter oldugunu gosterir
+##print(name[0:4])   0-4 arasinda ki karakteri yazdirir 3 dahil 4 dahil degil.
+## bosluk bir karakter olarak sayilir.
+##print(name[:4]) buda ayni sekilde 0-4 arasinda ki karakteri yazdirir 3 dahil 4 dahil degil
+##print(name.title()) ##bas harfleri buyuk yazar
+## print(name.upper))## tum yaziyi kucuk yazar 
+## print(name.lower)) ## tum yaziyi buyuk yazar 
+##print(name.count('a')) ## kac tane ornegin 'a' harfinden var gibi yani kac tane oldugunu yazdirir
+## print(name.find('n')) ##hangi karakterin kacinci sirada oldugunu gostermeye yarar
+##print(name.replace('ufuk bro' , 'adam')) ## burada hangi kelimeyi degistirmek istersek kullaniriz.
+##print(dir(name)) ##boyle yazdigimiz takdirde ilgili degiskenleri method ve ozellikleri bulabiliriz
+##print(help(str))## boyle yazdigimiz takdirde ilgili kullanbilcegimiz methodlari ve nelerde kullanabilecegimizi bize donen yardim dokumanidir
+##print(name[3]) ##bu sekilde yazarak kacinci karakterde hangi harf oldugunu bulur. 
+# unutmadan 0 ile basliyordu.
+ 
+
+
+
